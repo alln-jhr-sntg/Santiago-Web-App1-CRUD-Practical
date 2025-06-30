7f91751f94b7049bca8cb844e8130b588efab626
allenjherusantiago <allenjherussantiago@gmail.com>
2025-06-30 23:39:43 +0800

    spacing

--- db.php
+++ db.php
@@ -8,6 +8,6 @@
     $conn = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
-    die("Connection failed: " . $e->getMessage());
+        die("Connection failed: " . $e->getMessage());
     }
 ?>
